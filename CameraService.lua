9053df96a6b8944b3d4e7200baef0262248f3625
Synaptique <57877974+Alex78874@users.noreply.github.com>
2024-08-10 15:28:47 +0200

    Added LockCameraPanningPart()

--- CameraService.lua
+++ CameraService.lua
@@ -532,6 +532,25 @@ function CameraService:LockCameraPanning(lockXAxis: boolean, lockYAxis: boolean,
 	self.yLock = lockYAxis
 end
 
+=--> @LockCameraPanningPart: locks panning based on the look vector of a part.
+function CameraService:LockCameraPanningPart(lockXAxis: boolean, lockYAxis: boolean, Part: BasePart) 
+	assert(Part:IsA("BasePart"), "[CameraService] 3rd parameter should be a BasePart for :LockCameraPanning()")
+	
+	local lookVector = -Part.CFrame.LookVector
+	local angle = Vector2.new(
+		math.atan2(lookVector.X, lookVector.Z),
+		math.asin(-lookVector.Y)
+	)
+
+	--> Set up the camera orientation
+	self.atX = angle.X or 0
+	self.atY = angle.Y or 0
+
+	--> Lock if necessary
+	self.xLock = lockXAxis
+	self.yLock = lockYAxis
+end
+
 
 --> @SetCameraHost: for when you change the object the camera focuses on
 function CameraService:SetCameraHost(newHost: BasePart)
